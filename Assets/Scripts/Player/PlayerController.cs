48c2d71e0729eef54a1de090b3eab0c3657ab991
b3ardsy <b3ardsy@gmail.com>
2026-04-21 10:57:38 -0400

    Re-added aim, shoot, fall aim, jump aim.

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -3,26 +3,19 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody2D), typeof(Animator), typeof(SpriteRenderer))]
 public class PlayerController : MonoBehaviour
 {
-
-    //public GameObject groundCheckTransform;
     [SerializeField] private float groundCheckRadius = 0.02f;
     [SerializeField] private LayerMask groundLayer;
 
-    //Configurable variables
     [SerializeField] private float moveSpeed = 5f;
     [SerializeField] private float jumpForce = 5f;
 
-    //1. Poll our input so that we can see what our input values are.
-    //2. Move our player horizontally based on the horizontal input value.
     private Rigidbody2D rb;
     private Collider2D col;
     private SpriteRenderer sr;
     private Animator anim;
 
-    //cached variables
     private Vector2 groundCheckPos => CalculateGroundCheckPos();
 
-    //state variables
     private bool _isGrounded;
 
     private Vector2 CalculateGroundCheckPos()
@@ -31,43 +24,49 @@ public class PlayerController : MonoBehaviour
         return new Vector2(bounds.center.x, bounds.min.y);
     }
 
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         col = GetComponent<Collider2D>();
         sr = GetComponent<SpriteRenderer>();
         anim = GetComponent<Animator>();
-
     }
 
-    // Update is called once per frame
     void Update()
     {
         _isGrounded = Physics2D.OverlapCircle(groundCheckPos, groundCheckRadius, groundLayer);
 
         float horizontalInput = Input.GetAxisRaw("Horizontal");
-
         bool jumpInput = Input.GetButtonDown("Jump");
+        bool aimInput = Input.GetMouseButton(1);       // RMB held
+        bool shootInput = Input.GetMouseButtonDown(0); // LMB clicked
 
-        if (horizontalInput != 0) SpriteFlip(horizontalInput);
+        if (horizontalInput != 0)
+            SpriteFlip(horizontalInput);
 
-        // Move the player horizontally based on the horizontal input value
+        // Move player
         rb.linearVelocityX = horizontalInput * moveSpeed;
 
+        // Jump
         if (jumpInput && _isGrounded)
         {
             rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
         }
 
+        // Animator parameters
         anim.SetFloat("horizontalInput", Mathf.Abs(horizontalInput));
         anim.SetBool("isGrounded", _isGrounded);
         anim.SetFloat("yVel", rb.linearVelocityY);
+        anim.SetBool("isAiming", aimInput);
 
+        if (shootInput)
+        {
+            anim.SetTrigger("shoot");
+        }
     }
 
-    void SpriteFlip(float horizontalInput) => sr.flipX = (horizontalInput < 0);
-
-    
+    void SpriteFlip(float horizontalInput)
+    {
+        sr.flipX = (horizontalInput < 0);
+    }
 }
