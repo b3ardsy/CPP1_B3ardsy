b990d48641467c04a0e9be7e0f45d146b255b82e
b3ardsy <b3ardsy@gmail.com>
2026-04-23 18:49:59 -0400

    Ramps 'work'
    - walking up and down ramps works now
    - known issues: player cannot jump while walking up ramp else they get stuck in jump anim

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -3,9 +3,12 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody2D), typeof(Animator), typeof(SpriteRenderer))]
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private float groundCheckRadius = 0.02f;
+    [Header("Ground Check")]
     [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private float groundRayLength = 0.15f;
+    [SerializeField] private float maxSlopeAngle = 45f;
 
+    [Header("Movement")]
     [SerializeField] private float moveSpeed = 5f;
     [SerializeField] private float jumpForce = 5f;
 
@@ -14,15 +17,13 @@ public class PlayerController : MonoBehaviour
     private SpriteRenderer sr;
     private Animator anim;
 
-    private Vector2 groundCheckPos => CalculateGroundCheckPos();
+    private float horizontalInput;
+    private bool jumpPressed;
+    private bool aimInput;
+    private bool shootInput;
 
     private bool _isGrounded;
-
-    private Vector2 CalculateGroundCheckPos()
-    {
-        Bounds bounds = col.bounds;
-        return new Vector2(bounds.center.x, bounds.min.y);
-    }
+    private Vector2 groundNormal = Vector2.up;
 
     void Start()
     {
@@ -34,39 +35,95 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        _isGrounded = Physics2D.OverlapCircle(groundCheckPos, groundCheckRadius, groundLayer);
+        horizontalInput = Input.GetAxisRaw("Horizontal");
 
-        float horizontalInput = Input.GetAxisRaw("Horizontal");
-        bool jumpInput = Input.GetButtonDown("Jump");
-        bool aimInput = Input.GetMouseButton(1);       // RMB held
-        bool shootInput = Input.GetMouseButtonDown(0); // LMB clicked
+        if (Input.GetButtonDown("Jump"))
+            jumpPressed = true;
+
+        aimInput = Input.GetMouseButton(1);
+        shootInput = Input.GetMouseButtonDown(0);
 
         if (horizontalInput != 0)
             SpriteFlip(horizontalInput);
 
-        // Move player
-        rb.linearVelocityX = horizontalInput * moveSpeed;
-
-        // Jump
-        if (jumpInput && _isGrounded)
-        {
-            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
-        }
-
-        // Animator parameters
         anim.SetFloat("horizontalInput", Mathf.Abs(horizontalInput));
         anim.SetBool("isGrounded", _isGrounded);
         anim.SetFloat("yVel", rb.linearVelocityY);
         anim.SetBool("isAiming", aimInput);
 
         if (shootInput)
-        {
             anim.SetTrigger("shoot");
+    }
+
+    void FixedUpdate()
+    {
+        CheckGround();
+
+        Move();
+
+        if (jumpPressed && _isGrounded)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocityX, 0f);
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+
+        jumpPressed = false;
+    }
+
+    void CheckGround()
+    {
+        Bounds bounds = col.bounds;
+
+        Vector2 rayOrigin = new Vector2(bounds.center.x, bounds.min.y + 0.05f);
+
+        RaycastHit2D hit = Physics2D.Raycast(
+            rayOrigin,
+            Vector2.down,
+            groundRayLength,
+            groundLayer
+        );
+
+        if (hit.collider != null)
+        {
+            float slopeAngle = Vector2.Angle(hit.normal, Vector2.up);
+
+            _isGrounded = slopeAngle <= maxSlopeAngle;
+            groundNormal = hit.normal;
+        }
+        else
+        {
+            _isGrounded = false;
+            groundNormal = Vector2.up;
+        }
+
+        Debug.DrawRay(rayOrigin, Vector2.down * groundRayLength, Color.red);
+    }
+
+    void Move()
+    {
+        if (_isGrounded)
+        {
+            Vector2 slopeDirection = new Vector2(groundNormal.y, -groundNormal.x);
+
+            if (horizontalInput < 0)
+                slopeDirection *= -1;
+
+            rb.linearVelocity = new Vector2(
+                slopeDirection.x * Mathf.Abs(horizontalInput) * moveSpeed,
+                slopeDirection.y * Mathf.Abs(horizontalInput) * moveSpeed
+            );
+        }
+        else
+        {
+            rb.linearVelocity = new Vector2(
+                horizontalInput * moveSpeed,
+                rb.linearVelocityY
+            );
         }
     }
 
     void SpriteFlip(float horizontalInput)
     {
-        sr.flipX = (horizontalInput < 0);
+        sr.flipX = horizontalInput < 0;
     }
 }
